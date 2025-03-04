4711f514f4ca1d0031a64c69ae6c28e2e371ccb6
hitomi <lera4karyzhih@gmail.com>
2025-03-04 23:02:24 +0200

    виправила помилки

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,14 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new JavaCommunityBot("8051342115:AAEU-CKx0etwAipiADKKdG44yQ0fwp5e4yM"));
+            botsApi.registerBot(new JavaCommunityBot(""));
             System.out.println("Bot started successfully!");
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-
     }
 }
